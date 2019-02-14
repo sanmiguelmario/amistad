c366903304a3f0a334d2ba71d0eb967904243a4d
sanmiguelmario <42873283+sanmiguelmario@users.noreply.github.com>
2019-02-14 11:28:42 -0600

    Create mosaico_circulo.js

---
+++ mosaico_circulo.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background(255);
+	for ( var y= 5; y<=500; y= y+10){
+ for ( var x = 5; x<=500; x = x+10){
+	 
+    fill(0);
+  ellipse (x,y,10,10);
+
+	 }
+		}
+}
